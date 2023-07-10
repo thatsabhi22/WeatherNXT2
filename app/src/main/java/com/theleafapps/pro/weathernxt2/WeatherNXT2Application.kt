896d17d868e8cb85354cfa76dd24f938c86df5f6
abhijeet <redefinedabhi2@gmail.com>
2023-07-10 23:50:03 +0530

    Adding viewbinding support, WeatherActivity remained

--- app/src/main/java/com/theleafapps/pro/weathernxt2/WeatherNXT2Application.kt
+++ app/src/main/java/com/theleafapps/pro/weathernxt2/WeatherNXT2Application.kt
@@ -1,6 +1,7 @@
 package com.theleafapps.pro.weathernxt2
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class WeatherNXT2GenApplication : Application()
--- app/src/main/java/com/theleafapps/pro/weathernxt2/ui/WeatherActivity.kt
+++ app/src/main/java/com/theleafapps/pro/weathernxt2/ui/WeatherActivity.kt
@@ -3,8 +3,10 @@ package com.theleafapps.pro.weathernxt2.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.theleafapps.pro.weathernxt2.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
