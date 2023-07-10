d73eb2af0e7f3af167df9571cb7f162c56636bbf
abhijeet <redefinedabhi2@gmail.com>
2023-07-10 17:47:03 +0530

    Added Graphics and images
    Added weather page layout
    Created Application class

---
+++ app/src/main/java/com/theleafapps/pro/weathernxt2/WeatherNXT2Application.kt
@@ -0,0 +1,6 @@
+package com.theleafapps.pro.weathernxt2
+
+import android.app.Application
+
+@HiltAndroidApp
+class WeatherNXT2GenApplication : Application()
--- app/src/main/java/com/theleafapps/pro/weathernxt2/ui/MainActivity.kt
+++ app/src/main/java/com/theleafapps/pro/weathernxt2/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.theleafapps.pro.weathernxt2
+package com.theleafapps.pro.weathernxt2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.theleafapps.pro.weathernxt2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
